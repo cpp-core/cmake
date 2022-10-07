d6eb8114a30f2f98c3e3ac5432d39cb78be29b02
Mark Melton <mark.melton@notlem.com>
2022-10-07 17:33:18 -0400

    Added several external components.

--- recipes/add_spdlog.cmake
+++ recipes/add_spdlog.cmake
@@ -2,9 +2,6 @@ cmake_minimum_required (VERSION 3.24 FATAL_ERROR)
 
 include(${CMAKE_CURRENT_LIST_DIR}/add_component.cmake)
 
-macro(add_fmt)
-  set(FMT_INSTALL ON)
-  set(FMT_TEST OFF)
-  set(CMAKE_BUILD_TYPE Release)
-  add_component(fmt)
+macro(add_spdlog)
+  add_component(spdlog)
 endmacro()
