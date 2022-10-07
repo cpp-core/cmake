e40c55e66bde8d42d0f71b42d3ef81ba518e02fd
Mark Melton <mark.melton@notlem.com>
2022-10-07 17:47:10 -0400

    Updated add_cppzmq to include add_libzmq.

--- recipes/add_cppzmq.cmake
+++ recipes/add_cppzmq.cmake
@@ -3,5 +3,6 @@ cmake_minimum_required (VERSION 3.24 FATAL_ERROR)
 include(${CMAKE_CURRENT_LIST_DIR}/add_component.cmake)
 
 macro(add_cppzmq)
+  add_libzmq()
   add_component(cppzmq TARGET cppzmq-static)
 endmacro()
