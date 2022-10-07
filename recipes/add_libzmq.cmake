aa75191c6923647d6856f2bd0bcfa9e264b12a2f
Mark Melton <mark.melton@notlem.com>
2022-10-07 19:54:52 -0400

    Updated cmake add cpr and libzmq.

--- recipes/add_libzmq.cmake
+++ recipes/add_libzmq.cmake
@@ -3,6 +3,6 @@ cmake_minimum_required (VERSION 3.24 FATAL_ERROR)
 include(${CMAKE_CURRENT_LIST_DIR}/add_component.cmake)
 
 macro(add_libzmq)
-  add_component(ZeroMQ TARGET libzmq-static DIR libzmq)
   set(BUILD_TESTS OFF)
+  add_component(ZeroMQ TARGET libzmq-static DIR libzmq)
 endmacro()
