60349b1892b7368493ddde278ddd60a0239749cb
Mark Melton <mark.melton@notlem.com>
2022-10-08 08:22:01 -0400

    Updated cmake add_inja to find local json.

--- recipes/add_inja.cmake
+++ recipes/add_inja.cmake
@@ -3,5 +3,7 @@ cmake_minimum_required (VERSION 3.24 FATAL_ERROR)
 include(${CMAKE_CURRENT_LIST_DIR}/add_component.cmake)
 
 macro(add_inja)
+  add_json()
+  set(nlohmann_json_ROOT ${CPP_CORE_BINARY_DIR}/components/json)
   add_component(inja TARGET pantor::inja)
 endmacro()
