5f22da647e734e0c87b2d4c27f5a60f2cf6af2c5
Mark Melton <mark.melton@notlem.com>
2022-10-09 08:22:02 -0400

    Call enable_testing if gtest is added.

--- recipes/add_gtest.cmake
+++ recipes/add_gtest.cmake
@@ -3,5 +3,6 @@ cmake_minimum_required (VERSION 3.24 FATAL_ERROR)
 include(${CMAKE_CURRENT_LIST_DIR}/add_component.cmake)
 
 macro(add_gtest)
+  enable_testing()
   add_component(GTest TARGET GTest::gtest DIR googletest)
 endmacro()
