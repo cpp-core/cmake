8a8d6f1af78262060968304f4ed158a17f4523f7
Mark Melton <mark.melton@notlem.com>
2022-10-09 15:43:30 -0400

    Updated build.

--- recipes/add_gtest.cmake
+++ recipes/add_gtest.cmake
@@ -3,6 +3,7 @@ cmake_minimum_required (VERSION 3.24 FATAL_ERROR)
 include(${CMAKE_CURRENT_LIST_DIR}/add_component.cmake)
 
 macro(add_gtest)
+  include(GoogleTest)
   enable_testing()
   add_component(GTest TARGET GTest::gtest DIR googletest)
 endmacro()
--- recipes/add_xtensor.cmake
+++ recipes/add_xtensor.cmake
@@ -3,6 +3,7 @@ cmake_minimum_required (VERSION 3.24 FATAL_ERROR)
 include(${CMAKE_CURRENT_LIST_DIR}/add_component.cmake)
 
 macro(add_xtensor)
+  set(BUILD_BENCHMARK OFF)
   add_component(xtl)
   add_component(xsimd)
   add_component(xtensor)
