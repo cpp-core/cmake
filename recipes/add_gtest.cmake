94ed1bd19372dcae1e6d651536816c974385ec9f
Mark Melton <mark.melton@notlem.com>
2022-10-12 15:37:04 -0400

    WIP. cmake build.

--- recipes/add_gtest.cmake
+++ recipes/add_gtest.cmake
@@ -3,7 +3,7 @@ cmake_minimum_required (VERSION 3.24 FATAL_ERROR)
 function(add_gtest_impl)
   maybe_add_subdirectory(googletest)
   maybe_find_package(GTest)
-  maybe_fetch_content(googletest)
+  maybe_fetch_content(googletest TARGETS GTest::gtest)
   missing_error(GTest googletest ${CPP_CORE_DIR}/googletest)
 endfunction()
 
