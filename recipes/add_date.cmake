c22247d6fadd69a01e2599175ed1a42df3383299
Mark Melton <mark.melton@notlem.com>
2022-10-13 18:33:03 -0400

    WIP. cmake build.

--- recipes/add_date.cmake
+++ recipes/add_date.cmake
@@ -1,9 +1,13 @@
 cmake_minimum_required (VERSION 3.24 FATAL_ERROR)
 
-include(${CMAKE_CURRENT_LIST_DIR}/add_component.cmake)
-
 function(add_date)
+  return_if_target(date::date-tz)
+  
   set(USE_SYSTEM_TZ_DB ON CACHE INTERNAL "")
   set(BUILD_TZ_LIB ON CACHE INTERNAL "")
-  add_component(date TARGET date::date-tz)
+  maybe_add_subdirectory(date)
+  maybe_find_package(date)
+  maybe_fetch_content(date TAG master)
+
+  missing_error(date date ${CPP_CORE_DIR}/date)
 endfunction()
