b8ff74b27b43f496d37a2434b83a9afc10ebf8a1
Mark Melton <mark.melton@notlem.com>
2022-10-13 17:59:07 -0400

    WIP. cmake build.

--- recipes/add_json.cmake
+++ recipes/add_json.cmake
@@ -6,7 +6,7 @@ function(add_json)
   set(JSON_Install ON CACHE INTERNAL "Have json create the install target")
   maybe_add_subdirectory(json)
   maybe_find_package(nlohmann_json)
-  maybe_fetch_content(json TAG v3.11.2)
+  maybe_fetch_content(json TAG v3.11.2 TARGETS nlohmann_json::nlohmann_json)
 
   missing_error(json nlohmann_json::nlohmann_json ${CPP_CORE_DIR}/json)
 endfunction()
