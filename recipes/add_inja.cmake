80cfb668e393ab80588aab95276dbfc43327aa5e
Mark Melton <mark.melton@notlem.com>
2022-10-14 14:56:58 -0400

    WIP. cmake build.

--- recipes/add_inja.cmake
+++ recipes/add_inja.cmake
@@ -2,7 +2,7 @@ cmake_minimum_required (VERSION 3.24 FATAL_ERROR)
 
 function(add_inja)
   add_json()
-  set(nlohmann_json_ROOT ${CPP_CORE_BINARY_DIR}/components/json)
+  set(nlohmann_json_DIR ${CMAKE_BINARY_DIR}/_deps/json-build)
   set(BUILD_TESTING OFF)
   set(BUILD_BENCHMARK OFF)
   add_component(inja TARGETS pantor::inja)
